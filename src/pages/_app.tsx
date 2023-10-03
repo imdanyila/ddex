ed46caf519467f3eeb8518f6d07719ab7562ff6b
imdanyila <tiicdaniela@icloud.com>
2023-10-03 00:30:52 -0400

    set up login auth

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import { type AppType } from "next/app";
-
 import { api } from "npm/utils/api";
+import { ClerkProvider } from "@clerk/nextjs";
 
-import "npm/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ClerkProvider {...pageProps}>
+      <Component {...pageProps} />
+  </ClerkProvider>
+  );
 };
 
 export default api.withTRPC(MyApp);
