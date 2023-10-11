8656cc2afd9e363d47305d61721cbd80bc2ec30c
danielaromaguera <tiicdaniela@icloud.com>
2023-10-11 01:19:00 -0400

    fixing root file

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { exampleRouter } from "npm/server/api/routers/example";
+import { recipeRouter } from "npm/server/api/routers/example";
 import { createTRPCRouter } from "npm/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "npm/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  example: recipeRouter,
 });
 
 // export type definition of API
